2288a1febada22dda33eaf78f2e768617cc4ca4e
Gabriel Vasconcelos <gabriels.v9@gmail.com>
2025-12-27 15:47:00 -0300

    feat: cria mapeamento da entidade Cliente com NHibernate
    - ClienteMap usando ClassMapping do NHibernate
    - Mapeia todas as propriedades da entidade Cliente
    - Configura Cnpj usando CnpjUserType customizado

---
+++ GestaoClientes.Infrastructure/Persistence/NHibernate/Mappings/ClienteMap.cs
@@ -0,0 +1,56 @@
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using GestaoClientes.Domain.Entities;
+
+namespace GestaoClientes.Infrastructure.Persistence.NHibernate.Mappings;
+
+/// <summary>
+/// Mapeamento da entidade Cliente para o NHibernate.
+/// </summary>
+public class ClienteMap : ClassMapping<Cliente>
+{
+    public ClienteMap()
+    {
+        Table("Clientes");
+
+        Id(cliente => cliente.Id, mapeamento =>
+        {
+            mapeamento.Column("Id");
+            mapeamento.Generator(Generators.Guid);
+        });
+
+        Property(cliente => cliente.NomeFantasia, mapeamento =>
+        {
+            mapeamento.Column("NomeFantasia");
+            mapeamento.Length(200);
+            mapeamento.NotNullable(true);
+        });
+
+        Property(cliente => cliente.Cnpj, mapeamento =>
+        {
+            mapeamento.Column("Cnpj");
+            mapeamento.Length(14);
+            mapeamento.NotNullable(true);
+            mapeamento.Type<GestaoClientes.Infrastructure.Persistence.NHibernate.UserTypes.CnpjUserType>();
+        });
+
+        Property(cliente => cliente.Ativo, mapeamento =>
+        {
+            mapeamento.Column("Ativo");
+            mapeamento.NotNullable(true);
+        });
+
+        Property(cliente => cliente.DataCriacao, mapeamento =>
+        {
+            mapeamento.Column("DataCriacao");
+            mapeamento.NotNullable(true);
+        });
+
+        Property(cliente => cliente.DataAtualizacao, mapeamento =>
+        {
+            mapeamento.Column("DataAtualizacao");
+            mapeamento.NotNullable(false);
+        });
+    }
+}
+
